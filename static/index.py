1cae9a1ae64f5bf6e6db4bbac09c582efcba1c3b
tthe-the-j <119013593+tthe-the-j@users.noreply.github.com>
2022-12-12 00:42:20 +0000

    got pyscript button working

--- static/index.py
+++ static/index.py
@@ -1,22 +1,30 @@
 try:  # IDE workaround for imports
     from text_hider import TextHider
-    from pyscript import Element
+    from pyscript import Element, js, document
 except ModuleNotFoundError:  # in pyscript
     pass
 
 #  https://docs.pyscript.net/latest/tutorials/getting-started.html
 
-carrier_textarea = Element("encode-carrier")
-payload_textarea = Element("encode-payload")
-encode_button = Element("encode-button")
-encode_output = Element("encode-output")
+carrier_textarea = document.getElementById("encode-carrier")
+payload_textarea = document.getElementById("encode-payload")
+encode_button = document.getElementById("encode-button")
+encode_output = document.getElementById("encode-output")
 
 result = None
 
+test = None
+
 
 def encode(*args, **kwargs):
+    print("encode")
+    global test
+    test = 1
     carrier = carrier_textarea.value
     payload = payload_textarea.value
     global result
-    result = TextHider.hide(payload, carrier)
-    encode_output.write(result)
+    result = TextHider.hide(payload, carrier, ["0", "1"], 1)
+    encode_output.innerHTML = result
+
+
+encode_button.onclick = encode
