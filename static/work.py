5402546b8c0ee0ee156aadf45089280af1259ff1
the j <dfskndfsknvc knk @>
2023-01-16 09:37:32 +0000

    made button work

--- static/work.py
+++ static/work.py
@@ -6,14 +6,24 @@ except ModuleNotFoundError:  # in pyscript
 
 #  https://docs.pyscript.net/latest/tutorials/getting-started.html
 
-from js import method
-
 carrier_element = js.document.getElementById("carrier")
 payload_element = js.document.getElementById("payload")
+output_element = js.document.getElementById("output")
 
-def convert():
-    global method
+convert_button = js.document.getElementById("convert")
+
+
+def convert(*args):
+    print("convert")
+    from js import method
     if method == "hide":
         carrier = carrier_element.value
-        ...
-        #TextHider.hide(...)
+        payload = payload_element.value
+        allowed_characters = ["0", "1"]
+        index = 0
+        hidden = TextHider.hide(payload, carrier, allowed_characters, index)
+        print(hidden)
+        output_element.innerHTML = hidden
+
+
+convert_button.onclick = convert
