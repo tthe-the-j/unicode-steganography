4b9badcd0559b26cf77dc0262b9036b8308e32d9
tgfdvbvbcbvb <hbj@poo.com (real)>
2023-03-08 10:33:48 +0000

    added comments + looks nicer

--- static/text_hider.py
+++ static/text_hider.py
@@ -1,13 +1,14 @@
 import math
 import itertools
 from sympy import factorint
+
 # import textwrap
 
 
 class Encoding:
-    min_value : int
-    max_value : int
-    python_string : str
+    min_value: int
+    max_value: int
+    python_string: str
 
     @classmethod
     def is_valid(cls, character_code):
@@ -35,7 +36,6 @@ class UTF8(Encoding):
 
 
 class Payload:
-
     def __init__(self, text, characters, encoding, factor):
         self.text = text
         self.characters = characters
@@ -50,7 +50,9 @@ class Payload:
     @property
     def alphabet_count(self):
         if self._alphabet_count is None:
-            self._alphabet_count = len(list(filter(lambda c: 0x41 <= ord(c) <= 0x7a, self.text)))
+            self._alphabet_count = len(
+                list(filter(lambda c: 0x41 <= ord(c) <= 0x7A, self.text))
+            )
         return self._alphabet_count
 
 
@@ -60,44 +62,67 @@ class TextHider:
 
     @classmethod
     def hide(cls, payload, carrier, allowed_characters, index):
-        _hide = cls._get_hider(allowed_characters)
-        return _hide(payload, carrier, index)
+        # base to be encoded in
+        radix = len(allowed_characters)
+        # detect character set from payload
+        payload_character_set = cls._detect_character_set(payload)
+        # calculate number of digits needed per bit
+        num_of_digits = math.ceil(math.log(payload_character_set.max_value, radix))
+        # initialise encoded payload
+        encoded_payload = ""
+        for character in payload:
+            # get character code
+            character_code = ord(character)
+            # encode character
+            encoded_character = cls._base_n(
+                character_code, radix, allowed_characters
+            )
+            # calculate the length of padding needed
+            padding_length = num_of_digits - len(encoded_character)
+            # add padding to encoded character
+            encoded_character = (
+                padding_length * allowed_characters[0] + encoded_character
+            )
+            # add encoded character to encoded payload
+            encoded_payload += encoded_character
+        # insert encoded payload into carrier at index and return
+        return carrier[:index] + encoded_payload + carrier[index:]
 
     @classmethod
-    def show(cls, input_string, characters=None, start=None, end=None, possible_encodings=None):
+    def show(
+        cls,
+        input_string,
+        characters=None,
+        start=None,
+        end=None,
+        possible_encodings=None,
+    ):
         possible_encodings = possible_encodings or cls.encodings
         if characters is not None:
-            encoded = "".join((list(filter(lambda c: c in characters, list(input_string)))))
+            encoded = "".join(
+                (list(filter(lambda c: c in characters, list(input_string))))
+            )
         else:
             encoded = input_string[start:end]
-        possible_payloads = cls._get_possible_payloads(encoded, possible_encodings=possible_encodings)
+        possible_payloads = cls._get_possible_payloads(
+            encoded, possible_encodings=possible_encodings
+        )
         print(possible_payloads)
         return cls._most_possible_payload(possible_payloads)
 
-    """factory"""
-    @classmethod
-    def _get_hider(cls, allowed_characters):
-        def hide(payload, carrier, index):
-            radix = len(allowed_characters)
-            payload_character_set = cls._detect_character_set(payload)
-            num_of_digits = math.ceil(math.log(payload_character_set.max_value, radix))
-            encoded_payload = ""
-            for character in payload:
-                character_code = ord(character)
-                encoded_character = cls._base_n(character_code, radix, allowed_characters)
-                padding_length = num_of_digits - len(encoded_character)
-                encoded_character = padding_length*allowed_characters[0] + encoded_character
-                encoded_payload += encoded_character
-            return carrier[:index] + encoded_payload + carrier[index:]
-        return hide
-
+    # https://stackoverflow.com/questions/2267362/how-to-convert-an-integer-to-a-string-in-any-base
     @classmethod
     def _base_n(cls, num, radix, numerals):
+        # assert that the radix is the same as the number of numerals
         assert radix == len(numerals)
+        # recursion tail
         if num == 0:
             return numerals[0]
-        # https://stackoverflow.com/questions/2267362/how-to-convert-an-integer-to-a-string-in-any-base
-        return cls._base_n(num // radix, radix, numerals).lstrip(numerals[0]) + numerals[num % radix]
+        # calculate current digit and recurse to find the rest
+        return (
+            cls._base_n(num // radix, radix, numerals).lstrip(numerals[0])
+            + numerals[num % radix]
+        )
 
     @classmethod
     def _decode_base(cls, byte, characters):
@@ -109,12 +134,15 @@ class TextHider:
 
     @classmethod
     def _detect_character_set(cls, string):
+        # for each available character set
         for character_set in cls.encodings:
             try:
+                # try to encode string in character
                 string.encode(character_set.python_string)
                 return character_set
             except UnicodeEncodeError:
                 pass
+        # no available character set works
         raise UnicodeError(f"Can't detect character set: {string}")
 
     @classmethod
@@ -154,13 +182,13 @@ class TextHider:
                     for encoding, text in encoded_data.items():
                         possible_payloads.append(Payload(text, characters, encoding, f))
             possible_factors[f] = possible_character_permutations
-        #return possible_factors
+        # return possible_factors
         return possible_payloads
 
     @classmethod
     def _sort_payloads(cls, possible_payloads):
-        #letter_count = lambda text: len(list(filter(lambda c: 0x41 <= ord(c) <= 0x7a, text)))
-        #return sorted(list(itertools.chain.from_iterable([[[(letter_count(text), text, characters, factor) for encoding, text in j.items()] for characters, j in i.items()] for factor, i in possible_payloads.items()])))[::-1]
+        # letter_count = lambda text: len(list(filter(lambda c: 0x41 <= ord(c) <= 0x7a, text)))
+        # return sorted(list(itertools.chain.from_iterable([[[(letter_count(text), text, characters, factor) for encoding, text in j.items()] for characters, j in i.items()] for factor, i in possible_payloads.items()])))[::-1]
         return sorted(possible_payloads, key=lambda p: p.alphabet_count)[::-1]
 
     @classmethod
@@ -169,7 +197,7 @@ class TextHider:
 
 
 def wrap(text, n):
-    return [text[i:i + n] for i in range(0, len(text), n)]
+    return [text[i : i + n] for i in range(0, len(text), n)]
 
 
 if __name__ == "__main__":
